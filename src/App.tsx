2504cd0921ac4c8554bd01c3b2d762037c64fe25
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-07-07 00:17:33 +0300

    chore: add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app">
       <h1>Class-component task</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
-    </>
+    </div>
   );
 }
 
