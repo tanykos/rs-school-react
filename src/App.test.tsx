c4b45763dc08e1d2585ffd6aa818c89aecb387f8
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-07-15 13:29:35 +0300

    test: add Card Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,81 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { Paths } from './router/routesConstants';
+import userEvent from '@testing-library/user-event';
 
-test('renders search movies heading', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  const headingElement = screen.getByText(/search movies/i);
-  expect(headingElement).toBeInTheDocument();
-});
+const mockMovie = {
+  id: 'tt1234567',
+  title: 'Mock Movie',
+  year: '2021',
+  poster: 'mock-poster.jpg',
+};
 
-test('clicking on a card opens a detailed view', async () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
+const mockMovieDetails = {
+  imdbID: 'tt1234567',
+  Title: 'Mock Movie',
+  Year: '2021',
+  Genre: 'Action',
+  Country: 'USA',
+  Language: 'English',
+  Runtime: '120 min',
+  Actors: 'Actor 1, Actor 2',
+  Plot: 'This is a mock movie plot.',
+};
 
-  const loader = screen.getByText(/Loading/i);
+vi.mock('./services/apiService', () => ({
+  fetchItems: vi.fn(() => ({
+    results: [mockMovie],
+    totalPages: 1,
+  })),
+  fetchItemById: vi.fn((id) => {
+    if (id === 'tt1234567') {
+      return Promise.resolve(mockMovieDetails);
+    }
+    return Promise.resolve(null);
+  }),
+}));
 
-  expect(loader).toBeInTheDocument();
+describe('App Component', () => {
+  test('renders search movies heading', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const headingElement = screen.getByText(/search movies/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('check Loader is displayed', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const loader = screen.getByText(/Loading/i);
+
+    expect(loader).toBeInTheDocument();
+  });
+
+  test('clicking on a card opens a detailed card component', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path={Paths.HOME} element={<App />} />
+          <Route path={Paths.DETAILS} element={<div>Details Page</div>} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const cardElement = await waitFor(() => screen.getByText(/Mock Movie/i));
+    expect(cardElement).toBeInTheDocument();
+
+    userEvent.click(cardElement);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Details Page/i)).toBeInTheDocument();
+    });
+  });
 });
